070040b5981c1030b822f8c0d045443062434642
Dan Hentschel <dan@hentschels.com>
2017-03-31 06:42:39 -0400

    More experiments with custom messages

--- projects/Delegates/UnitTest.cs
+++ projects/Delegates/UnitTest.cs
@@ -14,13 +14,21 @@ namespace Delegates
         {
             Answers = null;
 
+            Console.WriteLine("CG> redirect-streams --input \"out\" \"null\"");
+            Console.WriteLine("CG> redirect-streams --input \"exercise results\" out");
+
             Example1.DoSayHello();
 
             Assert.IsNotNull(Answers, "You need to call Example1Runner.TestSayHello()");
             Assert.AreEqual("Hello, World!", Answers[0]);
+            Console.WriteLine("CG> message --channel \"exercise results\" IN: <World> OUT: <Hello, World!>");
             Assert.AreEqual("Hello, Dolly!", Answers[1]);
+            Console.WriteLine("CG> message --channel \"exercise results\" IN: <Dolly> OUT: <Hello, Dolly!>");
             Assert.AreEqual("Hello, there!", Answers[2]);
-            Console.WriteLine("CG> message You did it!");
+            Console.WriteLine("CG> message --channel \"exercise results\" IN: <there> OUT: <Hello, there!>");
+
+            Console.WriteLine("CG> message --channel \"exercise results\" \"\"");
+            Console.WriteLine("CG> message --channel \"exercise results\" You did it!");
         }
     }
 }
