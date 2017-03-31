41ce62ae441e88aaa4aec3ea67beddf8a9eda189
danBhentschel <dan@hentschels.com>
2017-03-31 11:55:18 -0400

    More compile errors

--- projects/Delegates/UnitTest.cs
+++ projects/Delegates/UnitTest.cs
@@ -8,6 +8,7 @@ namespace Delegates
     public class UnitTest
     {
         public static List<string> Answers;
+        private static string CgMessage = "CG> message -channel \"exercise results\"";
 
         [TestMethod]
         public void Exercise1()
@@ -15,21 +16,20 @@ namespace Delegates
             Answers = null;
 
             Example1.DoSayHello();
-            var cgMessage = "CG> message -channel \"exercise results\"";
 
-            Assert.IsNotNull(Answers, $"\n{cgMessage} You need to call Example1Runner.TestSayHello()");
+            Assert.IsNotNull(Answers, $"\n{CgMessage} You need to call Example1Runner.TestSayHello()");
             AssertAreEqual("Hello, World!", Answers[0], "World");
             AssertAreEqual("Hello, Dolly!", Answers[1], "Dolly");
             AssertAreEqual("Hello, there!", Answers[2], "there");
 
-            Console.WriteLine("CG> message --channel \"exercise results\" \"\"");
-            Console.WriteLine("CG> message --channel \"exercise results\" Congratulations, you did it!");
+            Console.WriteLine($"{CgMessage} \"\"");
+            Console.WriteLine($"{CgMessage> Congratulations, you did it!");
         }
 
         private static void AssertAreEqual(string expected, string actual, string provided)
         {
-            Assert.AreEqual(expected, actual, $"\n{cgMessage} EXPECTED: <{expected}>  GOT: <{actual}>");
-            Console.WriteLine($"{cgMessage} IN: <{provided}> OUT: <{actual}>");
+            Assert.AreEqual(expected, actual, $"\n{CgMessage} EXPECTED: <{expected}>  GOT: <{actual}>");
+            Console.WriteLine($"{CgMessage} IN: <{provided}> OUT: <{actual}>");
         }
     }
 }
