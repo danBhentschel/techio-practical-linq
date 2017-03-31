7b8793442ff2c8ea439d887ae2acc1a1970bc354
danBhentschel <dan@hentschels.com>
2017-03-31 11:05:06 -0400

    More runner experimentation

--- projects/Delegates/UnitTest.cs
+++ projects/Delegates/UnitTest.cs
@@ -14,21 +14,20 @@ namespace Delegates
         {
             Answers = null;
 
-            Console.WriteLine("CG> redirect-streams --input \"out\" \"null\"");
-            Console.WriteLine("CG> redirect-streams --input \"exercise results\" out");
+            Console.WriteLine("CG> message --channel \"execution progress\" Running...");
 
             Example1.DoSayHello();
 
             Assert.IsNotNull(Answers, "You need to call Example1Runner.TestSayHello()");
+            Console.WriteLine($"CG> message --channel \"exercise results\" IN: <World> OUT: <{Answers[0]}>");
             Assert.AreEqual("Hello, World!", Answers[0]);
-            Console.WriteLine("CG> message --channel \"exercise results\" IN: <World> OUT: <Hello, World!>");
+            Console.WriteLine($"CG> message --channel \"exercise results\" IN: <Dolly> OUT: <{Answers[1]}>");
             Assert.AreEqual("Hello, Dolly!", Answers[1]);
-            Console.WriteLine("CG> message --channel \"exercise results\" IN: <Dolly> OUT: <Hello, Dolly!>");
+            Console.WriteLine($"CG> message --channel \"exercise results\" IN: <there> OUT: <{Answers[2]}>");
             Assert.AreEqual("Hello, there!", Answers[2]);
-            Console.WriteLine("CG> message --channel \"exercise results\" IN: <there> OUT: <Hello, there!>");
 
             Console.WriteLine("CG> message --channel \"exercise results\" \"\"");
-            Console.WriteLine("CG> message --channel \"exercise results\" You did it!");
+            Console.WriteLine("CG> message --channel \"exercise results\" Congratulations, you did it!");
         }
     }
 }
