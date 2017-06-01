4d0a54cf31c15565d67a6c75710cc98a4ab0fb0a
Dan Hentschel <dan@hentschels.com>
2017-05-31 20:47:09 -0400

    Add exercise to lambda expressions

---
+++ projects/LinqExercises_sanitized/Background1/LambdaExpressions1.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Background1
+{
+    public static class LambdaExpressions1
+    {
+        // Write a lambda expression that will return the next number after
+        // the provided integer
+        public static Func<int, int> GetNextNumber = ??? => ???;
+    }
+}
